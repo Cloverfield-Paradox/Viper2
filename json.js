d97477ec52b55723d2866835d3648b3e814a9281
BuildTools <silence@lyreoss.com>
2021-04-21 01:08:06 -0700

    QoL
    Streamlined a few things and added the currently selected tags.
    -Put card array in a separate file. Still needs to be moved to a json.

---
+++ json.js
@@ -0,0 +1,92 @@
+//This is where all of the card data is stored. Needs to be moved to a separate json file when server is built
+var cardJSON = {
+    "ascent": [
+        { "color": "cyan" },
+        {
+            "map": "ascent",
+            "img": "images/cards/testCard1.png",
+            "desc": "A Execute Back of Generator",
+            "media":"images/cards/testcard1.png",
+            "type":"img",
+            "mDesc":"Molly for hitting the back of generator near A heaven",
+            "symbol": "C",
+            "tags": ["A Site","A Execute","Execute","Generator","Snakebite"]
+        },
+        {
+            "map": "ascent",
+            "img": "images/cards/testCard2.png",
+            "desc": "B Retake from A Ramp",
+            "media":"images/cards/testCard2.png",
+            "type":"img",
+            "mDesc":"lands on stairs, you can use a default snakebite to clear triple if you wanna use your 2nd snakebite as well.",
+            "symbol": "C",
+            "tags": ["B Site", "B Retake", "Retake", "Stairs", "Snakebite"]
+        },
+        {
+            "map": "ascent",
+            "img": "images/cards/testCard3_img.png",
+            "desc": "One Way Cat (Can't be picked up)",
+            "media":"images/cards/testCard3.mp4",
+            "type":"video",
+            "mDesc":"Orb cant be picked up",
+            "symbol": "Q",
+            "tags": ["Catwalk", "Mid", "Oneway", "Can't Pickup", "Orb"]
+        },
+        {
+            "map": "ascent",
+            "img": "images/cards/testCard3_img.png",
+            "desc": "One Way Cat (Can't be picked up)",
+            "media":"images/cards/testCard3.mp4",
+            "type":"video",
+            "mDesc":"Orb cant be picked up",
+            "symbol": "Q",
+            "tags": ["Catwalk", "Mid", "Oneway", "Can't Pickup", "Orb"]
+        }
+    ],
+    "bind": [
+        { "color": "cyan", },
+        {
+            "map": "bind",
+            "img": "images/cards/testCard4.png",
+            "desc": "B Site Hall Oneway",
+            "media":"images/cards/testCard4.png",
+            "type":"img",
+            "mDesc":"Oneway for the entrance to B from CT",
+            "symbol": "Q",
+            "tags": ["Oneway", "B Site", "Orb"]
+        },
+        {
+            "map": "bind",
+            "img": "images/cards/testCard4.png",
+            "desc": "B Site Hall Oneway",
+            "media":"images/cards/testCard4.png",
+            "type":"img",
+            "mDesc":"Oneway for the entrance to B from CT",
+            "symbol": "Q",
+            "tags": ["Oneway", "B Site", "Orb"]
+        },
+        {
+            "map": "bind",
+            "img": "images/cards/testCard4.png",
+            "desc": "B Site Hall Oneway",
+            "media":"images/cards/testCard4.png",
+            "type":"img",
+            "mDesc":"Oneway for the entrance to B from CT",
+            "symbol": "Q",
+            "tags": ["Oneway", "B Site", "Orb"]
+        },
+        {
+            "map": "bind",
+            "img": "images/cards/testCard2.png",
+            "desc": "B Retake from A Ramp",
+            "media":"images/cards/testCard2.png",
+            "type":"img",
+            "mDesc":"lands on stairs, you can use a default snakebite to clear triple if you wanna use your 2nd snakebite as well.",
+            "symbol": "C",
+            "tags": ["B Site", "B Retake", "Retake", "Stairs", "Snakebite"]
+        },
+    ],
+    "filter": [
+
+    ]
+}
